136c55e5650ddd639a858f4d7c8f5ffae72eaf38
thalq <95490979+thalq@users.noreply.github.com>
2022-11-07 21:36:57 +0300

    доработки

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -5,6 +5,7 @@ from flask import jsonify, request, url_for
 from . import app, db
 from .models import URL_map
 from .views import get_unique_short_id
+from .error_handlers import InvalidAPIUsage
 
 regex_url = '^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?$'
 regex_short_prefix = '[a-zA-Z0-9]+'
@@ -19,22 +20,18 @@ def to_dict(yacut):
 def get_original_link(short_id):
     yacut = URL_map.query.filter_by(short=short_id).first()
     if yacut is None:
-        return jsonify({'message':
-                        'Указанный id не найден'}), 404
+        raise InvalidAPIUsage('Указанный id не найден', 404)
     return jsonify({'url': yacut.original}), 200
 
 @app.route('/api/id/', methods=['POST'])
 def add_opinion():
     data = request.get_json()
     if 'original' not in data:
-        return jsonify({'message':
-                        '"url" является обязательным полем!'}), 400
+        raise InvalidAPIUsage('"url" является обязательным полем!')
     if re.fullmatch(regex_url, data['original']) is None:
-        return jsonify({'message':
-                        'Это некорректная ссылка'}), 400
+        raise InvalidAPIUsage('Это некорректная ссылка')
     if re.fullmatch(regex_short_prefix, data['short']) is None:
-        return jsonify({'message':
-                        'Указано недопустимое имя для короткой ссылки'}), 400
+        raise InvalidAPIUsage('Указано недопустимое имя для короткой ссылки')
     yacut = URL_map()
     for field in ['original', 'short']:
         if field not in data:
--- yacut/views.py
+++ yacut/views.py
@@ -16,18 +16,19 @@ def get_unique_short_id():
 @app.route('/', methods=['GET', 'POST'])
 def index_view():
     form = URLForm()
-    if not form.validate_on_submit():
-        return render_template('index.html', form=form)
-    custom_id=form.custom_id.data
-    if not custom_id:
-        custom_id = get_unique_short_id()
-    yacut = URL_map(
-        original=form.original_link.data,
-        short=custom_id,
-    )
-    db.session.add(yacut)
-    db.session.commit()
-    flash(url_for('short_link_view', short=custom_id, _external=True), "link")
+    if form.validate_on_submit():
+        custom_id=form.custom_id.data
+        if URL_map.query.filter_by(short=custom_id).first():
+            flash(f'Имя {custom_id} уже занято!')
+        if not custom_id:
+            custom_id = get_unique_short_id()
+        yacut = URL_map(
+            original=form.original_link.data,
+            short=custom_id,
+        )
+        db.session.add(yacut)
+        db.session.commit()
+        return (render_template('index.html', form=form, short=custom_id), 200)
     return render_template('index.html', form=form)
 
 @app.route('/<string:short>')
